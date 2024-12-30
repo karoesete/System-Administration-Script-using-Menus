1c3c2c0cc5f7ad67e8c60e86f53475033fc0e868
karoesete <esetekaro@gmail.com>
2024-12-30 01:13:59 +0000

    updated

--- sys_Admin_using_menus.sh
+++ sys_Admin_using_menus.sh
@@ -2,5 +2,5 @@
 PS3="Your choice: "
 select ITEM in "Add User" "List All Processes" "Kill Process" "Install Program" "Quit"
 do
-	echo "choose the right options now: "$ITEM
+	echo "choose the right opfasdfasdfasdfasdfsafddsafasfdtions now: "$ITEM
 done
